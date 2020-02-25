8099a99b659cd49eb1147df7d5ce388c3a3ddba3
becki-at-luminal <becki@luminal.io>
2020-02-25 15:48:34 -0500

    Add lock table

--- backend.tf
+++ backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "fugue-ci-cd-example-tfstate-XXXXXXXXXXXX-us-east-1"
     key    = "tfstate-aws/main.tfstate"
     region = "us-east-1"
+
+    dynamodb_table = "tfstate-lock"
   }
 }
