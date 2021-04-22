3581f4064f07cd8ea2f03c8cf51fb1976d18cbda
Becki Lee <becki@luminal.io>
2021-04-22 17:37:43 -0400

    Update CircleCI config, scan script, and README to use Fugue CLI instead of Fugue API

--- scan.sh
+++ scan.sh
@@ -1,34 +1,49 @@
 #!/bin/sh
 
+# Check for CLI env vars; print note about deprecated env vars
+if [ -z "$FUGUE_API_ID" ] || [ -z "$FUGUE_API_SECRET" ]; then
+   printf "\nFUGUE_CLIENT_ID and FUGUE_CLIENT_SECRET are deprecated. Please use FUGUE_API_ID and FUGUE_API_SECRET instead.\n"
+
+# If only deprecated env vars exist, use them
+   if [ ! -z "$FUGUE_CLIENT_ID" ] && [ ! -z "$FUGUE_CLIENT_SECRET" ]; then
+      export FUGUE_API_ID=$FUGUE_CLIENT_ID
+      export FUGUE_API_SECRET=$FUGUE_CLIENT_SECRET
+      printf "\nDetected FUGUE_CLIENT_ID and FUGUE_CLIENT_SECRET. Setting FUGUE_API_ID and FUGUE_API_SECRET to those values.\n"
+
+# If none detected, print error and exit
+   else
+      printf "\nError: No credentials detected. Please set the FUGUE_API_ID and FUGUE_API_SECRET credentials.\n"
+      exit 1
+   fi
+
+fi
+
 echo "Initiating Fugue scan..."
-curl -s -X POST https://api.riskmanager.fugue.co/v0/scans?environment_id=$FUGUE_ENV_ID \
-   -u $FUGUE_CLIENT_ID:$FUGUE_CLIENT_SECRET | jq '.' > scaninfo.json 
 
-SCAN_ID=$(jq -r '.id' scaninfo.json)
+# Scan environment and redirect output
+fugue scan $FUGUE_ENV_ID --wait --output json | jq '.' > scan_results.json
 
-echo "Fugue is now scanning your environment. Scan ID:"
-echo $SCAN_ID
+# Set scan ID env var
+SCAN_ID=$(jq -r '.id' scan_results.json)
 
-while [ "$(curl -s -X GET https://api.riskmanager.fugue.co/v0/scans/$SCAN_ID -u $FUGUE_CLIENT_ID:$FUGUE_CLIENT_SECRET | jq --raw-output '.status')" == "IN_PROGRESS" ]; do
-  printf "Scan in progress...\n"
-  sleep 15
-done
-
-curl -s -X GET https://api.riskmanager.fugue.co/v0/scans/$SCAN_ID -u $FUGUE_CLIENT_ID:$FUGUE_CLIENT_SECRET > scan_results.json
-
+# Set noncompliant resources env var
 NONCOMPLIANT=$(jq -r '.resource_summary.noncompliant' scan_results.json)
 
+# Set scan status env var
 SCAN_STATUS=$(jq -r '.status' scan_results.json)
 
+# Print results
 cat scan_results.json | jq '.'
 
+# If scan succeeds and any resources are noncompliant, get compliance by resource type, redirect output, and print noncompliant resources
 if [ "$SCAN_STATUS" == "SUCCESS" ] && [ "$NONCOMPLIANT" != "0" ]; then
-   curl -s -X GET https://api.riskmanager.fugue.co/v0/scans/$SCAN_ID/compliance_by_resource_types -u $FUGUE_CLIENT_ID:$FUGUE_CLIENT_SECRET > scan_results_noncompliant.json
+   fugue get compliance-by-resource-types $SCAN_ID --output json > scan_results_noncompliant.json
    printf "\nScan completed. Found $NONCOMPLIANT NONCOMPLIANT resource(s):\n\n"
    cat scan_results_noncompliant.json | jq -r '.items[].noncompliant[].resource_id'
 
+   # Update baseline with scan ID
    printf "\nUpdating baseline with scan ID $SCAN_ID..."
-   curl -s -X PATCH https://api.riskmanager.fugue.co/v0/environments/$FUGUE_ENV_ID -u $FUGUE_CLIENT_ID:$FUGUE_CLIENT_SECRET --data '{ "baseline_id": "'"$SCAN_ID"'" }' > update_baseline.json
+   fugue update env $FUGUE_ENV_ID --baseline-id $SCAN_ID --output json > update_baseline.json
    BASELINE_ID=$(jq -r '.baseline_id' update_baseline.json)
 
    if [ "$BASELINE_ID" == "$SCAN_ID" ]; then
@@ -42,10 +57,11 @@ if [ "$SCAN_STATUS" == "SUCCESS" ] && [ "$NONCOMPLIANT" != "0" ]; then
    printf "\nBuild failed. See scan_results_noncompliant.json for details."
    exit 1
 
+# If scan succeeds and all resources are compliant, print compliance message, update baseline with scan ID
 elif [ "$SCAN_STATUS" == "SUCCESS" ] && [ "$NONCOMPLIANT" == "0" ]; then
    printf "\nScan completed. All resources are compliant."
    printf "\nUpdating baseline with scan ID $SCAN_ID..."
-   curl -s -X PATCH https://api.riskmanager.fugue.co/v0/environments/$FUGUE_ENV_ID -u $FUGUE_CLIENT_ID:$FUGUE_CLIENT_SECRET --data '{ "baseline_id": "'"$SCAN_ID"'" }' > update_baseline.json
+   fugue update env $FUGUE_ENV_ID --baseline-id $SCAN_ID --output json > update_baseline.json
    BASELINE_ID=$(jq -r '.baseline_id' update_baseline.json)
 
    if [ "$BASELINE_ID" == "$SCAN_ID" ]; then
